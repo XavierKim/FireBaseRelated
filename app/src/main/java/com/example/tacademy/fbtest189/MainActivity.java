5519bf923eafad50f56c81b699f633298db13aee
C6-14\Tacademy <mouse_t@naver.com>
2017-07-10 12:04:13 +0900

    MainActivity에서 LoginActivity로 Intent 추가

--- app/src/main/java/com/example/tacademy/fbtest189/MainActivity.java
+++ app/src/main/java/com/example/tacademy/fbtest189/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.tacademy.fbtest189;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import com.example.tacademy.fbtest189.ui.LoginActivity;
 import com.google.firebase.iid.FirebaseInstanceId;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startActivity(new Intent(this, LoginActivity.class));
 
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
         Log.i("FCM","onTokenRefresh : "+ refreshedToken);
